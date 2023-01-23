298556cb9c3351e47c3e2a06de5f86b772d8e397
Tomas Martinez <tomas.martinez@xmartlabs.com>
2023-01-23 15:31:39 -0300

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,10 +8,12 @@ const BlogPage = ({data}) => {
     <Layout pageTitle="My Blog Posts">
       <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
       </ul>
@@ -21,13 +23,18 @@ const BlogPage = ({data}) => {
 
 export const query = graphql`
   query {
-    allFile(filter: {ext: {eq: ".mdx"}}) {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
-`
+`  
 
 export const Head = () => <Seo title="My Blog Posts" />
 
