9784ea1325bcc0e68644592b44b0ff58cad19f83
Tomas Martinez <tomas.martinez@xmartlabs.com>
2023-01-23 10:30:45 -0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+// Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from './layout'
+
+// Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle={"About Me"}>
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>
+    </main>
+  )
+}
+
+/* 
+
+  Gatsby lets you define a <title> and other document metadata with the Gatsby Head API. 
+  You have to export a component called Head from your page template to apply the metadata. 
+  Adding such metadata helps search engines like Google to better understand your site. 
+  For this tutorial you’ll only be adding titles to pages but you can also later add other metadata.
+
+*/
+export const Head = () => <title>About Me</title>
+
+// Export your component
+export default AboutPage
